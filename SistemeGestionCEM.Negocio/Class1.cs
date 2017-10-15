0d88027d8488b008a40f33280a5ea089095bb0fe
Sergio <sergio_95_31@hotmail.com>
2017-10-15 19:34:41 -0300

    Agrega los procedimientos almacenados, pero falta mapaerlos.

--- SistemeGestionCEM.Negocio/Class1.cs
+++ SistemeGestionCEM.Negocio/Class1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaGestionCEM.Datos;
 
 namespace SistemeGestionCEM.Negocio
 {
     public class Class1
     {
+        public void ejemplo()
+        {
+            using (Entities context = new Entities())
+            {
+                context.Database.SqlQuery<GENERO>("exec genero_crud.crear @p_cod_genero @p_descripcion", "1", "Macho");
+            }
+            
+        }
+
     }
 }
