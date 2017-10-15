69e4873d15c9bfb8a5aa54aad1d3caabe4539605
Sergio <sergio_95_31@hotmail.com>
2017-10-15 04:45:08 -0300

    Creación de las capas de negocio y de datos para cumplir con la arquitectura de capas

---
+++ SistemeGestionCEM.Negocio/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemeGestionCEM.Negocio
+{
+    public class Class1
+    {
+    }
+}
