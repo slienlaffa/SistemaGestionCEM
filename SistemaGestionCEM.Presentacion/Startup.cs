69be08b968bdfc3b641c7841e301ef7fb6ffd693
Sergio <sergio_95_31@hotmail.com>
2017-10-15 20:25:08 -0300

    Crea capa de presentación.

---
+++ SistemaGestionCEM.Presentacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaGestionCEM.Presentacion.Startup))]
+namespace SistemaGestionCEM.Presentacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
