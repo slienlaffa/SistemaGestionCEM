a6ec39edd714b7f850bcead78be0f412224e2b5c
Sergio <sergio_95_31@hotmail.com>
2017-10-16 18:45:12 -0300

    Arreglo de todos los problemas de conexion SE agrega un par de clases de prueba.

---
+++ SistemaGestionCEM.Datos/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionCEM.Datos
+{
+    public class Class1
+    {
+        public void ejemplo()
+        {
+            using (Entities context = new Entities())
+            {
+                String mensaje = context.GENERO.Find(1).DESCRIPCION;
+                Console.WriteLine(mensaje);
+            }
+        }
+    }
+}
---
+++ SistemaGestionCEM/prueba1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SistemaGestionCEM.Datos;
+
+namespace SistemaGestionCEM
+{
+    public class prueba1
+    {
+        public void ejemplo()
+        {
+            using (Entities context = new Entities())
+            {
+                String mensaje = context.GENERO.Find(2).DESCRIPCION;
+                Console.WriteLine(mensaje);
+            }
+        }
+
+    }
+}
