539a935bbddd94d6d70a5f6532796a768f4315a8
Sergio <sergio_95_31@hotmail.com>
2017-10-22 03:25:55 -0300

    Elimina carpeta proyecto de más

--- SistemaGestionCEM/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SistemaGestionCEM.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SistemaGestionCEM/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SistemaGestionCEM.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
