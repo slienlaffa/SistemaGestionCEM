1971efaa8da5ab23282a746e6b983b1c23223cbe
Sergio <sergio_95_31@hotmail.com>
2017-10-30 05:36:15 -0300

    CU11 Registrar notas de alumnos Metodo y prueba unitaria

---
+++ SistemaGestionCEM.Tests/CU11 - Registrar Notas de Alumnos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaGestionCEM.Negocio;
+using SistemaGestionCEM.Datos;
+
+namespace SistemaGestionCEM.Tests
+{
+    [TestClass]
+    public class CU11___Registrar_Notas_de_Alumnos
+    {   
+        [TestMethod]
+        public void RegistrarNotas()
+        {
+            EncargadoCEMNegocio cem = new EncargadoCEMNegocio();
+            var notas = cem.BuscarNotas(1);
+            foreach (var nota in notas)
+            {
+                nota.NOTA3 = 2;
+            }
+            bool resultado = cem.RegistrarNotas(notas);
+            Assert.IsTrue(resultado);
+        }
+    }
+}
